5b7594d21a255148fc18d856d595ff8fdf176579
GatoJustiniano <andres95liverpool@gmail.com>
2024-04-30 01:00:33 -0400

    add plantilla

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" 
+	class="dark-style layout-navbar-fixed layout-menu-fixed "
+	dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('/') }}"
+	data-template="vertical-menu-template-dark">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+	<title> @yield('title') | Dashboard - Gato Justiniano</title>
+
+	<!-- Favicon -->
+	<link rel="icon" type="image/vnd.microsoft.icon" href="{{ url('/favicon.ico') }}">
+
+	<!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+	<!-- Icons -->
+	<link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+	<!-- Core CSS -->
+	<link rel="stylesheet" href="{{ asset('css/bt5/core-dark.css') }}" class="template-customizer-core-css" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/theme-default-dark.css') }}" class="template-customizer-theme-css" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/demo.css') }}" />
+
+	<!-- Vendors CSS -->
+	<link rel="stylesheet" href="{{ asset('css/bt5/perfect-scrollbar.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/datatables-bootstrap5.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/responsive-bootstrap5.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/buttons-bootstrap5.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/select2.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/bt5/form-validation.css') }}" />
+
+	<!-- Page CSS -->
+	<link rel="stylesheet" href="{{ asset('css/page-auth.css') }}" />
+
+	<!-- Helpers -->
+	<script src="{{ asset('js/bt5/helpers.js') }}"></script>
+
+	<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+	<!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+	<script src="{{ asset('js/bt5/template-customizer.js') }}"></script>
+	<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+	<script src="{{ asset('js/bt5/config.js') }}"></script>
+
+	@stack('styles')
+</head>
+
+<body>
+
+	<!-- Layout wrapper -->
+	<div class="layout-wrapper layout-content-navbar  ">
+		<div class="layout-container">
+
+
+			<!-- Menu -->
+			@include('layouts.partials.aside-master')
+			<!-- / Menu -->
+
+
+			<!-- Layout container -->
+			<div class="layout-page">
+
+				<!-- Navbar -->
+				@include('layouts.partials.nav-header-master')
+				<!-- / Navbar -->
+
+
+
+				<!-- Content wrapper -->
+				<div class="content-wrapper">
+
+					<!-- Content -->
+
+					<div class="container-xxl flex-grow-1 container-p-y">
+						@include('layouts.partials.session-flash-status')
+
+						@yield('content')
+
+					</div>
+					<!-- / Content -->
+
+					<!-- Footer -->
+					@include('layouts.partials.footer-master')
+					<!-- / Footer -->
+
+					<div class="content-backdrop fade"></div>
+				</div>
+				<!-- Content wrapper -->
+			</div>
+			<!-- / Layout page -->
+		</div>
+
+		<!-- Overlay -->
+		<div class="layout-overlay layout-menu-toggle"></div>
+
+		<!-- Drag Target Area To SlideIn Menu On Small Screens -->
+		<div class="drag-target"></div>
+	</div>
+	<!-- / Layout wrapper -->
+
+
+	<!-- Core JS -->
+	<!-- build:js assets/vendor/js/core.js -->
+	<script src="{{ asset('js/bt5/libs/jquery.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/popper.js') }}"></script>
+	<script src="{{ asset('js/bt5/bootstrap.js') }}"></script>
+
+	<script src="{{ asset('js/bt5/libs/perfect-scrollbar.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/hammer.js') }}"></script>
+	<script src="{{ asset('js/bt5/menu.js') }}"></script>
+	
+	
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+	<!-- endbuild -->
+	
+	<!-- Vendors JS -->
+	<script src="{{ asset('js/bt5/libs/datatables-bootstrap5.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/form-validation-popular.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/form-validation-bootstrap5.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/form-validation-auto-focus.js') }}"></script>
+
+	<script src="{{ asset('js/bt5/libs/cleave.js') }}"></script>
+	<script src="{{ asset('js/bt5/libs/cleave-phone.js') }}"></script>
+
+	<!-- Main JS -->
+	<script src="{{ asset('js/bt5/main.js') }}"></script>
+
+	@stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/aside-master.blade.php
@@ -0,0 +1,94 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+
+    <div class="app-brand demo ">
+        <a href="/dashboard" class="app-brand-link">
+            <span class="app-brand-logo demo">
+                <img width="30px" src="{{ asset('img_logo/' . $settingGeneral->site_logo )}}" alt ="icon logo" />
+            </span>
+            <span class="app-brand-text demo menu-text fw-bolder ms-2">
+                {{ $settingGeneral->site_title }}
+            </span>
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+            <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboards -->
+        <li class="menu-item {{ in_array($activePage, ['users', 'proyects']) ? 'active open' : '' }} ">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon material-icons">assignment_turned_in</i>
+                Dashboards
+            </a>
+            <ul class="menu-sub">                
+                @can('user.index')
+                    <li class="menu-item {{ $activePage == 'users' ? ' active' : '' }}">
+                        <a href="{{ route('user.index') }}" class="menu-link">
+                            Usuarios
+                        </a>
+                    </li>
+                @endcan
+                @can('proyect.index')
+                    <li class="menu-item {{ $activePage == 'proyects' ? ' active' : '' }}">
+                        <a href=" {{ route('proyect.index') }} " class="menu-link">
+                            <div>Proyectos</div>
+                        </a>
+                    </li>
+                @endcan 
+            </ul>
+        </li>
+
+        <!-- Roles y permisos -->
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">Roles &amp; Permisos</span>
+        </li>
+        <li class="menu-item {{ in_array($activePage, ['roles', 'permissions']) ? 'active open' : '' }}">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-chart"></i>
+                <div>Roles y Permisos</div>
+            </a>
+            <ul class="menu-sub">
+                @can('roles.index')
+                    <li class="menu-item {{ $activePage == 'roles' ? ' active' : '' }}">
+                        <a href="{{ route('roles.index') }}" class="menu-link">
+                            <div>Roles</div>
+                        </a>
+                    </li>
+                @endcan
+                @can('permissions.index')    
+                    <li class="menu-item {{ $activePage == 'permissions' ? ' active' : '' }}">
+                        <a href="{{ route('permissions.index') }}" class="menu-link">
+                            <div>Permisos</div>
+                        </a>
+                    </li>
+                @endcan
+            </ul>
+        </li>
+        
+
+        <!-- Ayuda -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Ayuda</span></li>
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-receipt"></i>
+                <div>Tutorial</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="{{ route('setting.general') }}"
+                class="menu-link">
+                <i class="menu-icon tf-icons bx bx-cog"></i>
+                <div>Configuración general</div>
+            </a>
+        </li>
+    </ul>
+
+
+
+</aside>
